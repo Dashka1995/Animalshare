87f2f1402867e2ac22ee01ec40896da515d9b484
Dashka1995 <d0660260923@gmail.com>
2025-02-09 17:49:27 +0200

    drop-down menu completed

--- script.js
+++ script.js
@@ -2,22 +2,11 @@
 
 console.log(document.body.clientWidth);
 
-// window.addEventListener('resize', function () {
-//     if(document.body.clientWidth >= 834) {
-//         this.document.querySelector('.hero__greeting').classList.replace('h4-workSans', 'h3-workSans');
-//         this.document.querySelector('.hero__greeting').classList.replace('h1-workSans', 'h3-workSans');
-//     } if(document.body.clientWidth < 834) {
-//         this.document.querySelector('.hero__greeting').classList.replace('h3-workSans', 'h4-workSans');
-//         this.document.querySelector('.hero__greeting').classList.replace('h1-workSans', 'h4-workSans');
-//     } if(document.body.clientWidth >= 1280) {
-//         this.document.querySelector('.hero__greeting').classList.replace('h3-workSans', 'h1-workSans');
-//         this.document.querySelector('.hero__greeting').classList.replace('h4-workSans', 'h1-workSans');
-//     }
-// });
-
 document.querySelector(".btn-menu").addEventListener("click", function () {
   document.querySelector(".overlay").classList.toggle("hidden");
   document.querySelector(".mobile-menu").classList.toggle("hidden");
+  document.querySelector(".dropdown-menu--forum").classList.add("hidden");
+  document.querySelector(".dropdown-menu--marketplace").classList.add("hidden");
 });
 
 document
@@ -31,3 +20,13 @@ document.querySelector(".overlay").addEventListener("click", function () {
   document.querySelector(".mobile-menu").classList.add("hidden");
   document.querySelector(".overlay").classList.add("hidden");
 });
+
+document.querySelector(".mobile-menu__link-arrow--forum").addEventListener("click", function () {
+  document.querySelector(".dropdown-menu--forum").classList.toggle("hidden");
+});
+
+document.querySelector(".mobile-menu__link-arrow--marketplace").addEventListener("click", function () {
+  document.querySelector(".dropdown-menu--marketplace").classList.toggle("hidden");
+});
+
+
