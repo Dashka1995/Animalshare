98b81c59b2af988342b3cde8e2e3810ced096305
Dashka1995 <d0660260923@gmail.com>
2025-02-10 16:59:05 +0200

    completed the desktop header

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ console.log(document.body.clientWidth);
 document.querySelector(".btn-menu").addEventListener("click", function () {
   document.querySelector(".overlay").classList.toggle("hidden");
   document.querySelector(".mobile-menu").classList.toggle("hidden");
-  document.querySelector(".dropdown-menu--forum").classList.add("hidden");
-  document.querySelector(".dropdown-menu--marketplace").classList.add("hidden");
+  document.querySelector(".dropdown-menu--mobile-forum").classList.add("hidden");
+  document.querySelector(".dropdown-menu--mobile-marketplace").classList.add("hidden");
 });
 
 document
@@ -22,11 +22,11 @@ document.querySelector(".overlay").addEventListener("click", function () {
 });
 
 document.querySelector(".btn--whith-img--forum").addEventListener("click", function () {
-  document.querySelector(".dropdown-menu--forum").classList.toggle("hidden");
+  document.querySelector(".dropdown-menu--mobile-forum").classList.toggle("hidden");
 });
 
 document.querySelector(".btn--whith-img--marketplace").addEventListener("click", function () {
-  document.querySelector(".dropdown-menu--marketplace").classList.toggle("hidden");
+  document.querySelector(".dropdown-menu--mobile-marketplace").classList.toggle("hidden");
 });
 
 document.querySelector(".marketplace-sort__sorting-btn-open").addEventListener("click", function () {
